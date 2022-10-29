4afdff1e57c9cae4783ed03310cdf1c17537d991
Kooreis <kooreis.official@gmail.com>
2022-10-28 19:00:00 -0500

    Imported the Scanner class from the java.util package to read user input.

--- Text_Reverser.java
+++ Text_Reverser.java
@@ -1,13 +1,1 @@
-```java
 import java.util.Scanner;
-
-public class TextReverser {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter text to reverse:");
-        String input = scanner.nextLine();
-        String reversed = new StringBuilder(input).reverse().toString();
-        System.out.println("Reversed text: " + reversed);
-    }
-}
-```
