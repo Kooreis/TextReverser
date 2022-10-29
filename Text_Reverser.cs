9fa8af9cb65ae902e390e223f984826ba0db69a1
Kooreis <kooreis.official@gmail.com>
2022-10-28 19:00:00 -0500

    Initial setup of the program. It prompts the user to enter text.

--- Text_Reverser.cs
+++ Text_Reverser.cs
@@ -5,10 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter text: ");
-        string input = Console.ReadLine();
-        char[] charArray = input.ToCharArray();
-        Array.Reverse(charArray);
-        Console.WriteLine(new string(charArray));
-        Console.ReadKey();
     }
 }
